7c696c7b1731e15ad7a4f4afe88dc9c05380b9e3
mikitane <miika.luiro@etasku.fi>
2021-01-11 18:09:25 +0200

    Use DynamoDB from Lambda

--- services/shopping-lists/src/functions/createShoppingList.ts
+++ services/shopping-lists/src/functions/createShoppingList.ts
@@ -1,7 +1,35 @@
+import * as AWS from 'aws-sdk';
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { v4 as uuidv4 } from 'uuid';
 import 'source-map-support/register';
 
+const dynamoDB = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+
+
 export const createShoppingList: APIGatewayProxyHandler = async () => {
+  const uuid = uuidv4();
+
+  const params = {
+    TableName: process.env.MAIN_TABLE,
+    Item: {
+      'PK' : {S: uuid},
+      'SK' : {S: uuid},
+      'name': {S: 'My first shopping list'}
+    }
+  };
+
+  try {
+    await dynamoDB.putItem(params).promise();
+  } catch (e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Failed to create a new shopping list'
+      })
+    }
+  }
+
+
   return {
     statusCode: 201,
     body: JSON.stringify({
