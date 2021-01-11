299b0cafe911e1f934e13e6fcb54bd3fddf37aa6
mikitane <miika.luiro@etasku.fi>
2021-01-11 19:29:17 +0200

    WIP

--- services/shopping-lists/src/functions/getShoppingList.ts
+++ services/shopping-lists/src/functions/getShoppingList.ts
@@ -2,7 +2,7 @@ import * as AWS from 'aws-sdk';
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
+const dynamoDB = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
 
 
 export const getShoppingList: APIGatewayProxyHandler = async (event) => {
@@ -26,7 +26,10 @@ export const getShoppingList: APIGatewayProxyHandler = async (event) => {
     console.log('result', result);
     return {
       statusCode: 200,
-      body: JSON.stringify(result.$response.data),
+      body: JSON.stringify({
+        'result.$response.data': result.$response.data,
+        'result': result
+      }),
     };
   } catch (e) {
     console.error(e)
