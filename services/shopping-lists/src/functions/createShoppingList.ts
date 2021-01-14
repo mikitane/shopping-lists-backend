8b0e9b121d66b8017a590c84d1873a085d779208
Miika Luiro <32032940+mikitane@users.noreply.github.com>
2021-01-14 20:14:25 +0200

    Dynamo setup (#1)

--- services/shopping-lists/src/functions/createShoppingList.ts
+++ services/shopping-lists/src/functions/createShoppingList.ts
@@ -1,11 +1,42 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb'
+import { marshall } from '@aws-sdk/util-dynamodb'
 import { APIGatewayProxyHandler } from 'aws-lambda';
+import { v4 as uuidv4 } from 'uuid';
 import 'source-map-support/register';
 
-export const createShoppingList: APIGatewayProxyHandler = async () => {
+const dynamoDB = new DynamoDB({apiVersion: '2012-08-10'});
+
+export const createShoppingList: APIGatewayProxyHandler = async (event) => {
+  console.log('event', event)
+  const uuid = uuidv4();
+
+  const params = {
+    TableName: process.env.MAIN_TABLE,
+    Item: marshall({
+      pk : uuid,
+      sk : uuid,
+      name: 'My first shopping list!',
+    })
+  };
+
+  try {
+    await dynamoDB.putItem(params);
+  } catch (e) {
+    console.error(e)
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Failed to create a new shopping list'
+      })
+    }
+  }
+
+
   return {
     statusCode: 201,
     body: JSON.stringify({
-      message: 'Created a new shopping list!'
+      message: 'Created a new shopping list!',
+      id: uuid,
     }, null, 2),
   };
 }
---
+++ services/shopping-lists/src/functions/getShoppingList.ts
@@ -0,0 +1,53 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import 'source-map-support/register';
+
+// const dynamoDB = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+
+export const getShoppingList: APIGatewayProxyHandler = async () => {
+  // console.log('event', event)
+
+  // const uuid = event.pathParameters.id;
+
+  // console.log('uuid', uuid)
+
+
+  // const params = {
+  //   TableName: process.env.MAIN_TABLE,
+  //   Key: {
+  //     pk: uuid,
+  //     sk: uuid,
+  //   }
+  // };
+
+  // try {
+  //   const result = await dynamoDB.get(params).promise();
+  //   console.log('Result next')
+  //   console.log(result)
+  //   console.log('result', result);
+  //   console.log('result.ConsumedCapacity', result.ConsumedCapacity);
+  //   console.log('Object.keys(result)', Object.keys(result));
+
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({
+  //       'result.$response.data': result.$response.data,
+  //       'result': result
+  //     }),
+  //   };
+  // } catch (e) {
+  //   console.error(e)
+  //   return {
+  //     statusCode: 400,
+  //     body: JSON.stringify({
+  //       message: 'Failed to create a new shopping list!'
+  //     })
+  //   }
+  // }
+
+      return {
+      statusCode: 200,
+      body: JSON.stringify({message: "OK"}),
+    };
+}
+
