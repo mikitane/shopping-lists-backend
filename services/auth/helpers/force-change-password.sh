8d71773629aee7327e3eaed4741c0d61740c8ce6
mikitane <miika.luiro@etasku.fi>
2021-01-18 18:31:06 +0200

    WIP (Cognito)

---
+++ services/auth/helpers/force-change-password.sh
@@ -0,0 +1,15 @@
+# Initiate auth
+aws cognito-idp initiate-auth \
+--region eu-west-1 \
+--client-id 4uunk2r3mqvat88767pdcd7a36 \
+--auth-flow USER_PASSWORD_AUTH \
+--auth-parameters USERNAME=miika.luiro@outlook.com,PASSWORD=Jeejee123!
+
+# Respond to auth challenge
+aws cognito-idp respond-to-auth-challenge \
+--region eu-west-1 \
+--client-id 4uunk2r3mqvat88767pdcd7a36 \
+--challenge-name NEW_PASSWORD_REQUIRED \
+--challenge-responses USERNAME=<username>,NEW_PASSWORD=<password> \
+--session <session-id-from-initiate-auth>
+
