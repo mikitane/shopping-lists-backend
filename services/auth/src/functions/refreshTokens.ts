dc7ce5f1d526e10926d4e5e7150d5398b42ccbb6
mikitane <miika.luiro@etasku.fi>
2021-04-01 22:36:42 +0300

    Add logging

--- services/auth/src/functions/refreshTokens.ts
+++ services/auth/src/functions/refreshTokens.ts
@@ -12,6 +12,8 @@ export const refreshTokens: APIGatewayProxyHandler = async () => {
   // const command = AdminInitiateAuthCommand({
   //   ClientId: '2bl2qv4glv4vdonkles1o7agpo'
   // });
+  console.log('AdminMainUserPoolClientId', process.env.AdminMainUserPoolClientId);
+  console.log('AdminMainUserPoolClientSecret', process.env.AdminMainUserPoolClientSecret);
 
   return {
     statusCode: 200,
